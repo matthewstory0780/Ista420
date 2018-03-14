16248fb7e81cbdcf875215487e5e3150ad23d099
matthewstory0780 <36522359+matthewstory0780@users.noreply.github.com>
2018-03-14 09:52:46 -0400

    Delete SQLexercise3.sql

--- SQLexercises/SQLexercise3.sql
+++
@@ -1,8 +0,0 @@
-.echo on 
-.header on
-
-select od.orderid, od.productid, od.unitprice, od.quantity, (od.unitprice * od.quantity) 
-as LineTotal, 
-(select sum(od3.unitprice * od3.quantity) as OrderTotal 
-from order_details od3 where od3.orderid = od.orderid)
-as OrderTotal from order_details od limit 10;
